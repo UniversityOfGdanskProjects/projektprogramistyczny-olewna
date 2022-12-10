5dc3fffec250c91ff161a9085e9a362af96752b6
Oskar Lewna <dajcie.suba2002@gmail.com>
2022-12-10 21:34:56 +0100

    first commit, building structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return(
+        <div className="header">
+            <h1>
+                Coctail menu
+            </h1>
+        </div>
+    )
+}
